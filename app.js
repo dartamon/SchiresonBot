12093a3d381e890f31d97687e35e0c713f267167
Charles De Gennaro <admin@bciedu.us>
2017-02-08 13:14:06 -0500

    Restarted the bot

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-// Add your requirements
-var restify = require('restify'); 
-var builder = require('botbuilder'); 
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.PORT || 3000, function() 
-{
+server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
-
+  
 // Create chat bot
-var connector = new builder.ChatConnector
-({ appId: 'bd7a17f0-73bd-4a04-a08c-4eeffc6d4944', appPassword: 'toicpvOKrfshBGe8Snmpc3k' }); 
+var connector = new builder.ChatConnector({
+    appId: 'bd7a17f0-73bd-4a04-a08c-4eeffc6d4944',
+    appPassword: 'toicpvOKrfshBGe8Snmpc3k'
+});
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-// Create bot dialogs
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
 bot.dialog('/', function (session) {
     session.send("Hello World");
 });
-
-server.get('/', restify.serveStatic({
- directory: __dirname,
- default: '/index.html'
-}));
